15d7064f3bd496b258ee9f5798decb1fa69e73fe
nafish08 <md.nafish.sadik@gmail.com>
2022-05-27 14:12:28 +0600

    Environment variable created and connected with mongodb database

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oqxk0.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('booksHeaven').collection('book');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('Books Heaven is Running')
